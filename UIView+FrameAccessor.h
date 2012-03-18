d20ab99d8239410772465a35d98caea230e60ac6
Alex Denisov <1101.debian@gmail.com>
2012-03-18 18:23:07 +0200

    Initial and last commit =)

---
+++ UIView+FrameAccessor.h
@@ -0,0 +1,28 @@
+//
+//  UIView+FrameAccessor.h
+//  FrameAccessor
+//
+//  Created by Alex Denisov on 18.03.12.
+//  Copyright (c) 2012 CoreInvader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (FrameAccessor)
+
+- (CGPoint)origin;
+- (void)setOrigin:(CGPoint)newOrigin;
+- (CGSize)size;
+- (void)setSize:(CGSize)newSize;
+
+- (CGFloat)x;
+- (void)setX:(CGFloat)newX;
+- (CGFloat)y;
+- (void)setY:(CGFloat)newY;
+
+- (CGFloat)height;
+- (void)setHeight:(CGFloat)newHeight;
+- (CGFloat)width;
+- (void)setIwdth:(CGFloat)newWidth;
+
+@end
---
+++ UIView+FrameAccessor.m
@@ -0,0 +1,73 @@
+//
+//  UIView+FrameAccessor.m
+//  FrameAccessor
+//
+//  Created by Alex Denisov on 18.03.12.
+//  Copyright (c) 2012 CoreInvader. All rights reserved.
+//
+
+#import "UIView+FrameAccessor.h"
+
+@implementation UIView (FrameAccessor)
+
+- (CGPoint)origin {
+   return self.frame.origin;
+}
+
+- (void)setOrigin:(CGPoint)newOrigin {
+    CGRect newFrame = self.frame;
+    newFrame.origin = newOrigin;
+    self.frame = newFrame;
+}
+
+- (CGSize)size {
+    return self.frame.size;
+}
+
+- (void)setSize:(CGSize)newSize {
+    CGRect newFrame = self.frame;
+    newFrame.size = newSize;
+    self.frame = newFrame;
+}
+
+- (CGFloat)x {
+    return self.frame.origin.x;
+}
+
+- (void)setX:(CGFloat)newX {
+    CGRect newFrame = self.frame;
+    newFrame.origin.x = newX;
+    self.frame = newFrame;
+}
+
+- (CGFloat)y {
+    return self.frame.origin.y;
+}
+
+- (void)setY:(CGFloat)newY {
+    CGRect newFrame = self.frame;
+    newFrame.origin.y = newY;
+    self.frame = newFrame;
+}
+
+- (CGFloat)height {
+    return self.frame.size.height;
+}
+
+- (void)setHeight:(CGFloat)newHeight {
+    CGRect newFrame = self.frame;
+    newFrame.size.height = newHeight;
+    self.frame = newFrame;
+}
+
+- (CGFloat)width {
+    return self.frame.size.width;
+}
+
+- (void)setIwdth:(CGFloat)newWidth {
+    CGRect newFrame = self.frame;
+    newFrame.size.width = newWidth;
+    self.frame = newFrame;
+}
+
+@end
